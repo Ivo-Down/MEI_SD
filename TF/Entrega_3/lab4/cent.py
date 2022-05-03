6b0a56969a9a421e76367ea8ede99b0a0a62c508
Ivo-Down <ivo.baixo@gmail.com>
2022-05-03 20:21:24 +0100

    TF - entrega3- ponto 4 feito

--- TF/Entrega_3/lab4/cent.py
+++ TF/Entrega_3/lab4/cent.py
@@ -9,18 +9,62 @@ from db import DB
 logging.getLogger().setLevel(logging.DEBUG)
 
 db = DB()
+requestQueue = []  # Stores messages as we wait for their timestamp to come
+processQueue = {}  # Stores messages to process sorted by their timestamp | key: timeStamp, value: clientMsg
+nextTimeStamp = 0  # Next message to process
 
 
-def broadcast_transaction(msg):
+def broadcast_transaction(msg, timeStamp):
     global node_id, node_ids
     for n in node_ids:
-        send(node_id, n, type='txn_broadcast', payload=msg)
+        send(node_id, n, type='txn_broadcast', ts=timeStamp, payload=msg)
+
+
+# Checks if there is a new message to process and processes it if there is
+async def checkIfMessageToProcess():
+    global nextTimeStamp, processQueue
+    if nextTimeStamp in processQueue.keys():
+        clientMsg = processQueue[nextTimeStamp]
+        await processClientRequest(clientMsg)
+        # delete message from queue
+        del processQueue[nextTimeStamp]
+        nextTimeStamp += 1
+
     
+
+async def processClientRequest(clientMsg):
+    global node_id, db
+
+    # If the node is the one who was contacted by the client, reply to client
+    if clientMsg.dest == node_id:
+        ctx = await db.begin([k for op,k,v in clientMsg.body.txn], clientMsg.dest+'-'+str(clientMsg.body.msg_id))
+        rs,wv,res = await db.execute(ctx,clientMsg.body.txn)
+        if res:
+            await db.commit(ctx, wv)
+            reply(clientMsg, type='txn_ok', txn=res)
+        else:
+            reply(clientMsg, type='error', code=14, text='transaction aborted')
+        db.cleanup(ctx)
+
+    else:
+        ctx = await db.begin([k for op,k,v in clientMsg.body.txn], clientMsg.dest+'-'+str(clientMsg.body.msg_id))
+        rs,wv,res = await db.execute(ctx, clientMsg.body.txn)
+        if res:
+            await db.commit(ctx, wv)
+            send(node_id, clientMsg.dest, type='txn_broadcast_ok')
+        else:
+            send(node_id, clientMsg.dest, type='txn_broadcast_false')
+        db.cleanup(ctx)
+
+
 
 async def handle(msg):
     # State
     global node_id, node_ids
     global db
+    global requestQueue, processQueue, nextTimeStamp
+
+    await checkIfMessageToProcess()
 
     # Message handlers
     if msg.body.type == 'init':
@@ -33,33 +77,24 @@ async def handle(msg):
     
     elif msg.body.type == 'txn':
         logging.info('executing txn')
-        broadcast_transaction(msg)
+        requestQueue.append(msg)
+        send(node_id, 'lin-tso', type='ts')
 
 
     elif msg.body.type == 'txn_broadcast':
         logging.info('received broadcast')
-        bcast_msg = msg.body.payload
+        # Add new msg to processQueue
+        processQueue[msg.body.ts] = msg.body.payload
+        # If it contains the next message, processes it
+        #checkIfMessageToProcess()
 
-        # If the node is the one who was contacted by the client
-        if msg.src == node_id:
-          ctx = await db.begin([k for op,k,v in bcast_msg.body.txn], msg.src+'-'+str(msg.body.msg_id))
-          rs,wv,res = await db.execute(ctx,bcast_msg.body.txn)
-          if res:
-              await db.commit(ctx, wv)
-              reply(bcast_msg, type='txn_ok', txn=res)
-          else:
-              reply(bcast_msg, type='error', code=14, text='transaction aborted')
-          db.cleanup(ctx)
+            
 
-        else:
-          ctx = await db.begin([k for op,k,v in bcast_msg.body.txn], msg.src+'-'+str(bcast_msg.body.msg_id))
-          rs,wv,res = await db.execute(ctx, bcast_msg.body.txn)
-          if res:
-              await db.commit(ctx, wv)
-              send(node_id, msg.src, type='txn_broadcast_ok')
-          else:
-              send(node_id, msg.src, type='txn_broadcast_false')
-          db.cleanup(ctx)
+    elif msg.body.type == 'ts_ok':
+        timeStamp = msg.body.ts
+        msgToBroadcast = requestQueue.pop(0)
+        broadcast_transaction(msgToBroadcast, timeStamp)
+
 
 
     else:
