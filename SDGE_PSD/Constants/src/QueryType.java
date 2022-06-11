b4263106f5dd67bf01fcf82cab37ac431ab0434e
Pedro Pereira <pedromlmpereira98@gmail.com>
2022-06-11 23:42:27 +0100

    Reorganization

--- SDGE_PSD/Constants/src/QueryType.java
+++ SDGE_PSD/Constants/src/QueryType.java
@@ -1,4 +1,4 @@
-public final class QueryType {
+public class QueryType {
     public static final String QUERY_TOTAL_DEVICES_TYPE = "q_1"; // o número de dispositivos de um dado tipo online no sistema;
     public static final String QUERY_TOTAL_DEVICES = "q_2"; // o número de dispositivos ativos no sistema;
     public static final String QUERY_SPECIFIC_DEVICE = "q_3"; //  se um dado dispositivo (com um dado identificador) está online no sistema;
