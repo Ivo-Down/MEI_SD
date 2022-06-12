bef70b4890151009fe058cb3e58c55865e6f785a
Pedro Pereira <pedromlmpereira98@gmail.com>
2022-06-12 12:50:26 +0100

    Collector app changes and makefile

---
+++ SDGE_PSD/Erlang/projeto/src/Auxiliars/starter_app.erl
@@ -0,0 +1,40 @@
+%%%-------------------------------------------------------------------
+%% @doc starter public API
+%% @end
+%%%-------------------------------------------------------------------
+
+-module(starter_app).
+-behaviour(application).
+-export([start/2, stop/1]).
+
+-define(NumberOfRegions, 5).
+-define(DevicesFileName, "dispositivos_10000.json").
+-define(BaseCollectorPort, 1233).
+-define(BaseAggregatorPort, 8300).
+
+start(_StartType, _StartArgs) ->
+    {CollectorList, AggList} = portList(?NumberOfRegions, [], []),
+    io:fwrite("Result ~p ~p \n", [CollectorList, AggList]),
+    login_manager:start(?DevicesFileName),
+    start_collectors(CollectorList, AggList),
+    devices:start(CollectorList, ?DevicesFileName),
+    starter_sup:start_link().
+
+start_collectors([],[]) ->
+    done;
+start_collectors([CollPort|CT], [AggPort|AT]) ->
+    io:fwrite("SC ~p ~p \n",[CollPort,AggPort]),
+    coletor:start(CollPort, AggPort, ?DevicesFileName),
+    start_collectors(CT, AT).
+
+portList(0, CollList, AggList) -> {CollList, AggList};
+portList(NumberOfRegions, CollList, AggList) ->
+    NewCollPort = ?BaseCollectorPort + NumberOfRegions,
+    NewAggPort = ?BaseAggregatorPort + NumberOfRegions,
+    portList(NumberOfRegions-1, [NewCollPort] ++ CollList,[NewAggPort] ++ AggList).
+        
+
+stop(_State) ->
+    ok.
+
+%% internal functions
